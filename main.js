a4dc5b6157dc4edc7314d8693475575da76a55bf
fgaticadev <fgaticadev@gmail.com>
2022-09-18 15:03:48 -0300

    toogle mobile menu

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 console.log("ejecutando script");
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 menuEmail.addEventListener('click', toggleDesktopMenu)
+mobileMenuIcon.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
-    console.log("click");
+    console.log('click desktop');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    console.log('click mobile');
+    mobileMenu.classList.toggle('inactive');
 }
